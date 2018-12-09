1490123d192a8911e18c221e48ba8978c2d9ff49
Yikai Guan <kk940725@126.com>
2018-12-09 22:28:52 +0100

    added firebase to index.htmland main.js #1

---
+++ main.js
@@ -0,0 +1,9 @@
+    // Initialize Firebase
+    firebase.initializeApp({
+        apiKey: "AIzaSyCub7oG2LMQBoK6CisyZAQLlQtHSzAgvEk",
+        authDomain: "mushroom-project-1544209434551.firebaseapp.com",
+        databaseURL: "https://mushroom-project-1544209434551.firebaseio.com",
+        projectId: "mushroom-project-1544209434551",
+        storageBucket: "mushroom-project-1544209434551.appspot.com",
+        messagingSenderId: "64814358483"
+    });
